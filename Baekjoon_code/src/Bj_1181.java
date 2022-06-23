5de7f94cc25d4492bf1c0555b53c757046d2d536
Mark <qudfbftnqls@naver.com>
2022-06-23 23:11:18 +0900

    220622 baekjoon 1Q solve

---
+++ Baekjoon_code/src/Bj_1181.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bj_1181 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] arr = new String[n];
+		
+		for (int i=0; i<n; i++) {
+			arr[i] = sc.next();
+		}
+		Arrays.sort(arr);
+		
+		if (n>1) {
+			for (int j=1; j<=50; j++) {
+				for (int i=0; i<n; i++) {
+					if (i==n-1) {
+						if (arr[i].length() == j) {
+							System.out.println(arr[i]);
+						}
+					} else {
+						if (arr[i].length() == j && !(arr[i].equals(arr[i+1]))) {
+							System.out.println(arr[i]);
+						}
+					}
+				}
+			}
+		} else if (n==1) {
+			System.out.println(arr[0]);
+		}
+		sc.close();
+	}
+}

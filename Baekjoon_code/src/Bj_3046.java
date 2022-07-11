8593ee62caa1b6d78ec216853eee081de38c5079
Mark <qudfbftnqls@naver.com>
2022-07-12 00:02:57 +0900

    220711 baekjoon 1Q solve

---
+++ Baekjoon_code/src/Bj_3046.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Bj_3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(-1*sc.nextInt() + 2*sc.nextInt());
+		sc.close();
+	}
+}

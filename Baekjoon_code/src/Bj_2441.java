459014414607c34d0ce5469bd28e8eb095a9d2ea
Mark <qudfbftnqls@naver.com>
2022-07-26 07:52:20 +0900

    220725 baekjoon 2Q solve

---
+++ Baekjoon_code/src/Bj_2441.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Bj_2441 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for (int i=n; i>0; i--) {
+			for (int j=i; j<n; j++) {
+				System.out.print(" ");
+			}
+			for (int k=i; k>0; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}

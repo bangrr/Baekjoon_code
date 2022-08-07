233c00e7406344ce1cba2e3212305eecffa273a9
Mark <qudfbftnqls@naver.com>
2022-08-08 00:01:49 +0900

    220807 baekjoon 4Q solve
    3003 5339 5522 8370

---
+++ Baekjoon_code/src/Bj_5522.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Bj_5522 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		for (int i=0; i<5; i++) {
+			int a = sc.nextInt();
+			sum += a;
+		}
+		System.out.print(sum);
+		sc.close();
+	}
+}
---
+++ Baekjoon_code/src/Bj_8370.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Bj_8370 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextInt()*sc.nextInt() + sc.nextInt()*sc.nextInt());
+		sc.close();
+	}
+}

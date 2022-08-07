881551dff6c64f1a7d5523a3a869efff8f46f07a
Mark <qudfbftnqls@naver.com>
2022-08-07 23:33:18 +0900

    220806 baekjoon 1Q solve
    5338

---
+++ Baekjoon_code/src/Bj_5338.java
@@ -0,0 +1,9 @@
+public class Bj_5338 {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-;;-._|\n"
+				+ "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}

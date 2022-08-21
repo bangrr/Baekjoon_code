51fd99c8421028b8319ee02f2e385cc3a90cfd6d
Mark <qudfbftnqls@naver.com>
2022-08-22 02:30:28 +0900

    220821 baekjoon 1Q solve
    16483

---
+++ Baekjoon_code/src/Bj_16483.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Bj_16483 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(Math.round(Math.pow(sc.nextDouble()/2, 2)));
+		sc.close();
+	}
+}

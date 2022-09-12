bccd7783ca4c55fe139c1cbdbf9401e1a567ddb9
Mark <qudfbftnqls@naver.com>
2022-09-12 21:15:32 +0900

    220911 baekjoon 1Q solve
    15439

---
+++ Baekjoon_code/src/Bj_15439.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Bj_15439 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		System.out.println(n*(n-1));
+		sc.close();
+	}
+}

02c2374709fc2f5c9d24149032f6fc896d912e89
Mark <qudfbftnqls@naver.com>
2022-09-15 00:05:17 +0900

    220914 baekjoon 1Q solve
    21598

---
+++ Baekjoon_code/src/Bj_21598.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bj_21598 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("SciComLove\n".repeat(n));
+		sc.close();
+	}
+}

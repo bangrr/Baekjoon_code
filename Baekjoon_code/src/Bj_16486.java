810ce36c6c72243f611c4d4ca34b3d2e1afe8f89
Mark <qudfbftnqls@naver.com>
2022-09-22 00:10:59 +0900

    220921 baekjoon 1Q solve
    16486

---
+++ Baekjoon_code/src/Bj_16486.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Bj_16486 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(2*sc.nextInt() + 2*3.141592*sc.nextInt());
+		sc.close();
+	}
+}

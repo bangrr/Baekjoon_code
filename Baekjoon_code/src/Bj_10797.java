8fd9bd3ebb53a8d09c33bc7ec73581fc748dc3aa
Mark <qudfbftnqls@naver.com>
2022-09-30 00:20:34 +0900

    220929 baekjoon 1Q solve
    10797

---
+++ Baekjoon_code/src/Bj_10797.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Bj_10797 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		
+		for (int i=0; i<5; i++) {
+			if (n == sc.nextInt()) {
+				cnt++;
+			}
+		}
+		System.out.print(cnt);
+		sc.close();
+	}
+}

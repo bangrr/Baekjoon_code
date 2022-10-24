f06942e7403e0301ffc5411d48741416da1ca365
Mark <qudfbftnqls@naver.com>
2022-10-25 07:03:08 +0900

    221024 baekjoon 1Q solve
    2839

---
+++ Baekjoon_code/src/Bj_2839.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Bj_2839 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int cnt = 0;
+		
+		if (n == 4 || n == 7) {
+			cnt = -1;
+		} else if (n % 5 == 0) {
+			cnt = n / 5;
+		} else if (n % 5 == 1 || n % 5 == 3) {
+			cnt = n / 5 + 1;
+		} else if (n % 5 == 2 || n % 5 == 4) {
+			cnt = n / 5 + 2;
+		}
+		System.out.print(cnt);
+		sc.close();
+	}
+}

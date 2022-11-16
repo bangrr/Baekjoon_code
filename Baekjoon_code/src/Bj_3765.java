c2595cd42e7c0ae367cda2d7a74b6f0e0b2e4dc6
Mark <qudfbftnqls@naver.com>
2022-11-17 00:14:35 +0900

    221116 baekjoon 1Q solve
    3765

---
+++ Baekjoon_code/src/Bj_3765.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Bj_3765 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		String s = "";
+		while ((s = br.readLine()) != null) {
+			sb.append(s).append("\n");
+		}
+		System.out.print(sb);
+	}
+}

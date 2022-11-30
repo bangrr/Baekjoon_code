68f978eb38eb958f18b8e5a61a274daf2ce59818
Mark <qudfbftnqls@naver.com>
2022-12-01 00:42:26 +0900

    221130 baekjoon 1Q solve
    15680

---
+++ Baekjoon_code/src/Bj_15680.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Bj_15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n == 0) {
+			System.out.print("YONSEI");
+		} else {
+			System.out.print("Leading the Way to the Future");
+		}
+		sc.close();
+	}
+}

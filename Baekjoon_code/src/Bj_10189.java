b7e5a69b2e8b58911d56f104a980d761fd8214a7
Mark <qudfbftnqls@naver.com>
2022-12-22 00:11:59 +0900

    221221 baekjoon 1Q solve
    10189

---
+++ Baekjoon_code/src/Bj_10189.java
@@ -0,0 +1,8 @@
+public class Bj_10189 {
+	public static void main(String[] args) {
+		System.out.println("#  # #### #### #  #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#### #  # #  # # #\n"
+				+ "#  # #### #### #  #");
+	}
+}

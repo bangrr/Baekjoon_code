9ba8883183f009001a3270586fdf433a4c5fa9b5
Mark <qudfbftnqls@naver.com>
2022-12-27 00:04:06 +0900

    221226 baekjoon 1Q solve
    26489

---
+++ Baekjoon_code/src/Bj_26489.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Bj_26489 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int c = 0;
+		while (br.readLine() != null) {
+			c++;
+		}
+		System.out.print(c);
+	}
+}

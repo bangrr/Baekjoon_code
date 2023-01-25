088d79d2020bdeefca6ef5bad2ec547a96bfcfec
Mark <qudfbftnqls@naver.com>
2023-01-26 00:41:06 +0900

    230125 baekjoon 1Q solve
    11282

---
+++ Baekjoon_code/src/Bj_11282.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Bj_11282 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print((char) (Integer.parseInt(br.readLine())-1 + '가'));
+	}
+}

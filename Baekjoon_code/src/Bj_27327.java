109be038fbcef169192aa38ac3e139edeec0aae8
Mark <qudfbftnqls@naver.com>
2023-02-02 00:36:08 +0900

    230201 baekjoon 1Q solve
    27327

---
+++ Baekjoon_code/src/Bj_27327.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Bj_27327 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(24*sc.nextInt());
+		sc.close();
+	}
+}

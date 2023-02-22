fb43194e2597589789a4feb7a4c3fb266d5ba4f7
Mark <qudfbftnqls@naver.com>
2023-02-23 06:41:54 +0900

    230222 baekjoon 1Q solve
    10813

---
+++ Baekjoon_code/src/Bj_10813.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_10813 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n+1];
+		for (int i=1; i<n+1; i++) {
+			arr[i] = i;
+		}
+		
+		while (m-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			arr[0] = arr[i];
+			arr[i] = arr[j];
+			arr[j] = arr[0];
+		}
+		
+		for (int i=1; i<n+1; i++) {
+			sb.append(arr[i]).append(" ");
+		}
+		
+		System.out.print(sb.toString());
+	}
+}

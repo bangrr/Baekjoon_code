74825c4c814a3532751ff0cf3b0443e0d4698cc4
Mark <qudfbftnqls@naver.com>
2023-02-27 00:28:32 +0900

    230226 baekjoon 1Q solve
    1463 17356

---
+++ Baekjoon_code/src/Bj_1463.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Bj_1463 {
+	
+	static Integer[] dp; 
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int x = Integer.parseInt(br.readLine());
+		
+		dp = new Integer[x+1];
+		dp[0] = dp[1] = 0;
+		
+		System.out.print(recur(x));
+	}
+	
+	static int recur(int x) {
+		if (dp[x] == null) {
+			if (x % 6 == 0) {
+				dp[x] = Math.min(recur(x - 1), Math.min(recur(x / 3), recur(x / 2))) + 1;
+			} else if (x % 3 == 0) {
+				dp[x] = Math.min(recur(x / 3), recur(x - 1)) + 1;
+			} else if (x % 2 == 0) {
+				dp[x] = Math.min(recur(x / 2), recur(x - 1)) + 1;
+			} else {
+				dp[x] = recur(x - 1) + 1;
+			}
+		}
+		return dp[x];
+	}
+}
---
+++ Baekjoon_code/src/Bj_17356.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Bj_17356 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(1/(1+Math.pow(10, (-sc.nextDouble()+sc.nextDouble())/400)));
+	}
+}

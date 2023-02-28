f636bb36a2a45c81b8ff9db2358268724c12a7e2
Mark <qudfbftnqls@naver.com>
2023-03-01 00:11:57 +0900

    230228 baekjoon 1Q solve
    11719

---
+++ Baekjoon_code/src/Bj_11719.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Bj_11719 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		
+		while ((s = br.readLine()) != null) {
+			System.out.println(s);
+		}
+	}
+}

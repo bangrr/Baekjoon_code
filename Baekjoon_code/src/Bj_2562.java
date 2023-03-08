e863caf3211a80ef2c9c49a07b4a1547f7d043c2
Mark <qudfbftnqls@naver.com>
2023-03-09 00:11:56 +0900

    230308 baekjoon 1Q fix
    2562

--- Baekjoon_code/src/Bj_2562.java
+++ Baekjoon_code/src/Bj_2562.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Bj_2562 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] a = new int[9];
 		
 		int max = 0;
 		int idx = 0;
-		for (int i=0; i<a.length; i++) {
-			a[i] = sc.nextInt();
-			if (max < a[i]) {
-				max = a[i];
+		for (int i=0; i<9; i++) {
+			int tmp = sc.nextInt();
+			if (max < tmp) {
+				max = tmp;
 				idx = i+1;
 			}
 		}

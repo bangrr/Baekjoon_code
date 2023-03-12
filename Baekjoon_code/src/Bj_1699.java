77e207636ac0c87dabc74c98fc2bb8b556852feb
Mark <qudfbftnqls@naver.com>
2023-03-12 23:34:40 +0900

    230310 baekjoon 2Q solve
    1699 17626

---
+++ Baekjoon_code/src/Bj_1699.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Bj_1699 {
+	static int[] memo;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		memo = new int[n+1];
+		
+		for (int i=1; i*i<=n; i++) { // 제곱수는 1로 기록
+			memo[i*i] = 1;
+		}
+		System.out.print(dp(n));
+	}
+	
+	static int dp(int n) {
+		if (memo[n] == 0) {
+			memo[n] = n;
+			for (int i = (int)Math.sqrt(n); i >= 0; i--) {
+				memo[n] = Math.min(dp(n - i*i) + 1, memo[n]);
+			}
+		}
+		return memo[n];
+	}
+}

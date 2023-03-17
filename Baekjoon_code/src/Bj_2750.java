7ec6a3ee78fc159753db0f43e4a7459fe66646a0
Mark <qudfbftnqls@naver.com>
2023-03-17 10:13:26 +0900

    fix Baekjoon 2750

--- Baekjoon_code/src/Bj_2750.java
+++ Baekjoon_code/src/Bj_2750.java
@@ -1,18 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Bj_2750 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] a = new int[n];
-		for(int i=0; i<n; i++) {
-			a[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		for (int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		sc.close();
-		Arrays.sort(a);
-		for(int i=0; i<n; i++) {
-			System.out.println(a[i]);
+//		Arrays.sort(arr);
+		// sort() 메소드 안쓰고 버블 정렬 구현
+		for (int i=0; i<n-1; i++) {
+			for (int j=0; j<n-1-i; j++) {
+				if (arr[j] > arr[j+1]) {
+					int tmp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = tmp;
+				}
+			}
 		}
+		// 출력 부분
+		for (int i=0; i<n; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+		System.out.print(sb.toString());
 	}
 }

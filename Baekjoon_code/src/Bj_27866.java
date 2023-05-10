18445615dccf8764806f706cce19273029dca284
Mark <qudfbftnqls@naver.com>
2023-05-10 23:11:55 +0900

    [ADD] 27866

---
+++ Baekjoon_code/src/Bj_27866.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Bj_27866 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println(s.charAt(sc.nextInt()-1));
+		sc.close();
+	}
+}

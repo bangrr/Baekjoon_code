d00fcbdd842a27f7cc046b1dacf815b203bf10a9
Mark <qudfbftnqls@naver.com>
2023-05-31 01:29:16 +0900

    [ADD] 19698

---
+++ Baekjoon_code/src/Bj_19698.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_19698 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		input();
+	}
+	
+	// 입력부
+	static void input() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		
+		System.out.print(Math.min(N, (W/L) * (H/L)));
+	}
+}

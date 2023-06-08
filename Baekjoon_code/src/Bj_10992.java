97ec47c7d975c7179f6472283fd7a3298f5370d5
Mark <qudfbftnqls@naver.com>
2023-06-08 22:03:15 +0900

    [ADD] 10992 별 찍기 - 17

--- Baekjoon_code/src/Bj_10992.java
+++ Baekjoon_code/src/Bj_10992.java
@@ -10,37 +10,20 @@ public class Bj_10992 {
 	// main 메소드
 	public static void main(String[] args) throws IOException {
 		int n = Integer.parseInt(br.readLine());
-		int c = (1<<n)-1;
-		if (n % 2 == 1) {
-			for (int i = 1; i <= c; i++) {
-				for (int j = 1; j <= c - 1 + i; j++) {
-					if (i == c) {
-						sb.append("*");
-					} else if (j == c + 1 - i) {
-						sb.append("*");
-					} else if (j == c - 1 + i) {
-						sb.append("*");
-					} else {
-						sb.append(" ");
-					}
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - 1 + i; j++) {
+				if (i == n) {
+					sb.append("*");
+				} else if (j == n + 1 - i) {
+					sb.append("*");
+				} else if (j == n - 1 + i) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
 				}
-				sb.append("\n");
 			}
-		} else {
-			for (int i = c; i >= 1; i--) {
-				for (int j = 1; j <= c - 1 + i; j++) {
-					if (i == c) {
-						sb.append("*");
-					} else if (j == c + 1 - i) {
-						sb.append("*");
-					} else if (j == c - 1 + i) {
-						sb.append("*");
-					} else {
-						sb.append(" ");
-					}
-				}
-				sb.append("\n");
-			}
+			sb.append("\n");
 		}
 		System.out.print(sb.toString());
 	}

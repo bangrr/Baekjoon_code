55e03f57058d3b3fa64e2da3787294e0b80c7090
Mark <qudfbftnqls@naver.com>
2023-06-10 21:40:15 +0900

    [ADD] 10990 별 찍기 - 15

---
+++ Baekjoon_code/src/Bj_10990.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_10990 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - 1 + i; j++) {
+				if (j == n + 1 - i) {
+					sb.append("*");
+				} else if (j == n - 1 + i) {
+					sb.append("*");
+				} else {
+					sb.append(" ");
+				}
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb.toString());
+	}
+}

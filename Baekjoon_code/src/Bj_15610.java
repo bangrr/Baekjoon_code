297ccfff14fcd3b43d2ae5164645e6ae64e65d0f
Mark <qudfbftnqls@naver.com>
2023-07-03 00:03:08 +0900

    [ADD] 15610 Abbey Courtyard
    정사각형 넓이가 주어지면 둘레를 구하는 문제

---
+++ Baekjoon_code/src/Bj_15610.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_15610 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		System.out.println(Math.sqrt(Long.parseLong(br.readLine()))*4);
+	}
+}

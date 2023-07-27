bd92b026bf57bf349c269af8872c71cd053c3973
Mark <qudfbftnqls@naver.com>
2023-07-27 23:15:51 +0900

    [ADD] 14935

---
+++ Baekjoon_code/src/Bj_14935.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_14935 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		/* 모든 수에 대해서 FA만 출력이 된다*/
+		System.out.print("FA");
+	}
+}

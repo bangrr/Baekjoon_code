92f67c03b0d434b2dc0531d733e7d28d013f36fc
Mark <qudfbftnqls@naver.com>
2023-08-15 21:32:45 +0900

    [ADD] 28444

---
+++ Baekjoon_code/src/Bj_28444.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Bj_28444 {
+	// 입출력 메소드
+	static Scanner sc = new Scanner(System.in);
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+ 
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		System.out.print(sc.nextInt()*sc.nextInt() - sc.nextInt()*sc.nextInt()*sc.nextInt());
+	}
+}

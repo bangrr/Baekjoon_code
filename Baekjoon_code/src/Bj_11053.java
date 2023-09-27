ff6e85bb0407171f7134ed269041013f0b40af0e
Mark <qudfbftnqls@naver.com>
2023-09-28 02:44:45 +0900

    [ADD] 11053

---
+++ Baekjoon_code/src/Bj_11053.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_11053 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n+1];
+		int[] dp = new int[n+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=1; i<=n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1;
+		}
+		
+		int max = 1;
+		for (int i=1; i<=n; i++) {
+			for (int j=1; j<i; j++) {
+				if (arr[j] < arr[i]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+}

f17826751f62f4512bbcbdda51e07125aca7058e
Mark <qudfbftnqls@naver.com>
2023-10-02 08:42:58 +0900

    [ADD] 1402

---
+++ Baekjoon_code/src/Bj_1402.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Bj_1402 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+ 
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		int t = Integer.parseInt(br.readLine());
+		while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            System.out.println("yes");
+        }
+    }
+}

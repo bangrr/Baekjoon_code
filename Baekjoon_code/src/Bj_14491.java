a818b6e8f9e2ba2b6432ff6fbd452f88c36f8970
Mark <qudfbftnqls@naver.com>
2023-10-19 23:52:33 +0900

    [ADD] 14491

---
+++ Baekjoon_code/src/Bj_14491.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_14491 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		int t = Integer.parseInt(br.readLine());
+		System.out.println(Integer.toString(t, 9));
+	}
+}

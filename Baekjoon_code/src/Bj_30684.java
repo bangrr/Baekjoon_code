c62231f4725398cbd2566b6c7bf2e3e830769cdf
Mark <qudfbftnqls@naver.com>
2023-11-24 00:05:47 +0900

    [ADD] 30684

---
+++ Baekjoon_code/src/Bj_30684.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Bj_30684 {
+	// 입출력 메소드
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+ 
+	// main 메소드
+	public static void main(String[] args) throws IOException {
+		int n = Integer.parseInt(br.readLine());
+        String m = "ZZZ";
+		while (n-- > 0) {
+            String s = br.readLine();
+            if (s.length()==3 && m.compareTo(s)>0) m = s;
+        }
+        System.out.print(m);
+	}
+}

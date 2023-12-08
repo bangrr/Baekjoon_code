a49e6ab7860fa678d9403c023c88f808c78e9311
Mark <qudfbftnqls@naver.com>
2023-12-08 15:36:56 +0900

    [UPD] 15552

--- Baekjoon_code/src/Bj_15552.java
+++ Baekjoon_code/src/Bj_15552.java
@@ -1,26 +1,21 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Bj_15552 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int t = Integer.parseInt(br.readLine());
-		
 		for (int i=0; i<t; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			bw.write(a+b + "\n");
+			sb.append(a+b).append("\n");
 		}
-		bw.flush();
-		bw.close();
+		System.out.print(sb);
 	}
 }

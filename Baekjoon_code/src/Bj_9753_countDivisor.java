dcb91b15042960e43a0ba217b3d353885cc46dda
Mark <qudfbftnqls@naver.com>
2023-12-13 14:02:29 +0900

    [ADD] 9753 countDivisor
    (다른 분 풀이 참조) k의 약수를 세는 방법으로 푼 방법

--- Baekjoon_code/src/Bj_9753_countDivisor.java
+++ Baekjoon_code/src/Bj_9753_countDivisor.java
@@ -17,16 +17,16 @@ public class Bj_9753_countDivisor {
 	static void input() throws IOException {
 		int t = Integer.parseInt(br.readLine());
 		while (t-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			while (solve(n) != 4) n++;
-			sb.append(n).append("\n");
+			int k = Integer.parseInt(br.readLine());
+			while (solve(k) != 4) k++;
+			sb.append(k).append("\n");
 		}
 	}
 	
 	// 문제해결
-	static int solve(int n) {
+	static int solve(int k) {
 		int ret = 0;
-		for (int i=1; i<=n; i++) if (n % i == 0) ret++;
+		for (int i=1; i<=k; i++) if (k % i == 0) ret++;
 		return ret;
 	}
 }

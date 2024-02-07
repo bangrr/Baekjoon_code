c7ae6c35cfd65d406fe94523ad47110e5b7f1f55
Mark <qudfbftnqls@naver.com>
2024-02-07 23:08:54 +0900

    [ADD] 30620
    연산횟수가 최소가 될 필요가 없기 떄문에
    x -> x*y -> y 순서가 되도록 연산을 해준다

---
+++ src/Bj_30620.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj_30620 {
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        long x = rd.nextLong();
+        long y = rd.nextLong();
+        long mul = x*y;
+        System.out.println(2);
+        System.out.println(mul-x);
+        System.out.println(y-mul);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

966916d380cd24f3a06c479d55bebfb59f33f939
Mark <qudfbftnqls@naver.com>
2024-02-08 23:32:51 +0900

    [ADD] 2075
    우선순위 큐 문제

---
+++ src/Bj_2075.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj_2075 {
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        int n = rd.nextInt();
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                pq.add(rd.nextInt());
+            }
+        }
+        int ans = 0;
+        while (n-- > 0) {
+            ans = pq.poll();
+        }
+        System.out.print(ans);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

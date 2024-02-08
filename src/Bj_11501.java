b1604d21dad9b24ece84d5139b640ba987b54a20
Mark <qudfbftnqls@naver.com>
2024-02-08 23:31:29 +0900

    [ADD] 11501
    뒤에서부터 최댓값일때 앞에꺼를 판다

---
+++ src/Bj_11501.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj_11501 {
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        StringBuilder sb = new StringBuilder();
+        int t = rd.nextInt();
+        while (t-- > 0) {
+            int n = rd.nextInt();
+            int[] arr = new int[n];
+            long ans = 0;
+            for (int i=0; i<n; i++) {
+                arr[i] = rd.nextInt();
+            }
+
+            int max = arr[n-1];
+            for (int i=n-2; i>=0; i--) {
+                if (arr[i] >= max) {
+                    max = arr[i];
+                } else {
+                    ans += (max-arr[i]);
+                }
+            }
+            sb.append(ans).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

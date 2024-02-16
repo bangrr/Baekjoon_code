2b5dcc934a54d3b7d07267ea2b453b877efedcda
Mark <qudfbftnqls@naver.com>
2024-02-16 23:02:15 +0900

    [ADD] 31306

---
+++ src/Bj_31306.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj_31306 {
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        StringBuilder sb = new StringBuilder();
+
+        int x = 0;
+        int y = 0;
+        String s = rd.nextLine();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') x++;
+            if (c == 'y') y++;
+        }
+        System.out.print(x + " " + (x+y));
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

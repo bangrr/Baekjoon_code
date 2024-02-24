1503d013552d88bbd7e779646150eb6f6acc102a
Mark <qudfbftnqls@naver.com>
2024-02-25 00:11:28 +0900

    [ADD] 25400

---
+++ src/Bj_25400.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Bj_25400 {
+    public static void main(String[] args) {
+        FastReader rd = new FastReader();
+        StringBuilder sb = new StringBuilder();
+
+        int n = rd.nextInt();
+        int p = 1;
+        int a = 0;
+        for (int i=0; i<n; i++) {
+            if (rd.nextInt() != p) {
+                a++;
+            } else {
+                p++;
+            }
+        }
+        System.out.print(a);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while(st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}

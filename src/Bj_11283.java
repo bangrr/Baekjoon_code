db2c95d6ed88fbaa0fcdb36334dce7b30a3a9dfa
Mark <qudfbftnqls@naver.com>
2024-05-30 21:07:36 +0900

    [ADD] 11283

---
+++ src/Bj_11283.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Bj_11283 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().charAt(0)-'가'+1);
+    }
+}

93d1611ab4ed7d12b1c4741aef97a2b576ccc8a7
Mark <qudfbftnqls@naver.com>
2024-06-19 01:52:06 +0900

    [ADD] 18242

--- src/Bj_18242.java
+++ src/Bj_18242.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Bj_16204 {
+public class Bj_18242 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;

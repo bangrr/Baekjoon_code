b720e8add75ff898ff801d26d2559860cf33e110
Mark <qudfbftnqls@naver.com>
2024-08-29 21:50:07 +0900

    [ADD] 32193

---
+++ src/Bj_32193.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_32193 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int d = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = a-b;
+            sb.append(d+=c).append("\n");
+        }
+        System.out.print(sb);
+    }
+}

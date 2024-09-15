1ecd62f4efcdd9b385ed709c2742c5fc6e1839a1
Mark <qudfbftnqls@naver.com>
2024-09-15 22:58:31 +0900

    [ADD] 24072

---
+++ src/Bj_24072.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_24072 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.print(a <= c && c < b ? 1 : 0);
+    }
+}

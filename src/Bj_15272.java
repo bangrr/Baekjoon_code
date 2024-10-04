70fb1ebdf5ab0975673908bcb64a5fb902898996
Mark <qudfbftnqls@naver.com>
2024-10-04 22:31:17 +0900

    [ADD] 15272

---
+++ src/Bj_15272.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_15272 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        System.out.print(br.readLine().contains("ss") ? "hiss" : "no hiss");
+    }
+}

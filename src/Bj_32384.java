a7621f0b96657a91eabc1855a4527645d3891ec6
Mark <qudfbftnqls@naver.com>
2024-10-06 15:20:25 +0900

    [ADD] 32384

---
+++ src/Bj_32384.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_32384 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print("LoveisKoreaUniversity ".repeat(n));
+    }
+}

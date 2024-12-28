e8cbe592f7ec9e96b6fdd058dfcb8d7fc8e59599
Mark <qudfbftnqls@naver.com>
2024-12-28 23:23:26 +0900

    [ADD] 11000

---
+++ src/Bj_11000.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_11000 {
+    static class Lecture {
+        int start, end;
+
+        public Lecture(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        Lecture[] lectures = new Lecture[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
+            lectures[i] = new Lecture(s, t);
+        }
+
+        Arrays.sort(lectures, (l1, l2) -> l1.start == l2.start ? l1.end - l2.end : l1.start - l2.start);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(lectures[0].end);
+
+        for (int i = 1; i < n; i++) {
+            if (pq.peek() <= lectures[i].start) {
+                pq.poll();
+            }
+            pq.add(lectures[i].end);
+        }
+        System.out.print(pq.size());
+    }
+}

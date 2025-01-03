0ad523baa694b5ba653eb96f33475d42fcd57823
Mark <qudfbftnqls@naver.com>
2025-01-04 08:03:19 +0900

    [ADD] 11966

---
+++ src/Bj_11966.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_11966 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        boolean flag = true;
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                flag = false;
+                break;
+            }
+        }
+        System.out.print(flag ? 1 : 0);
+    }
+}

c01ae733791b4b8ad9cc788726653a616bdcfe0d
Mark <qudfbftnqls@naver.com>
2025-01-07 22:29:44 +0900

    [ADD] 14920

---
+++ src/Bj_14920.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_14920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int c = 1;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            c++;
+        }
+        System.out.print(c);
+    }
+}

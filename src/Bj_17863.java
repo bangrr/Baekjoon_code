25a81a400cd3b050bb9b31fde9f19fc432765615
Mark <qudfbftnqls@naver.com>
2025-02-02 22:39:40 +0900

    [ADD] 17863

---
+++ src/Bj_17863.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_17863 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String s = br.readLine();
+        System.out.print(s.startsWith("555") ? "YES" : "NO");
+    }
+}

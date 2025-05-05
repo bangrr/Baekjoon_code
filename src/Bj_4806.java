f423a8a4c395ab0260ef7f33613e64349a97002c
Mark <qudfbftnqls@naver.com>
2025-05-05 22:47:52 +0900

    [ADD] 4806

---
+++ src/Bj_4806.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Bj_4806 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = 0;
+        while ((br.readLine()) != null) {
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}

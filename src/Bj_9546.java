ab835df266066b9f213c9b5d4a661822bda29b83
박병률 <bangrr25@entropykorea.com>
2025-05-16 10:24:05 +0900

    [ADD] 9546

---
+++ src/Bj_9546.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Bj_9546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            System.out.println((int) Math.pow(2, k) - 1);
+        }
+    }
+}

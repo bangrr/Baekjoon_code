e1953104556b34c6caa69d4e39a730abf8336ce3
박병률 <bangrr25@entropykorea.com>
2025-07-03 08:40:39 +0900

    [ADD] 21553

---
+++ src/Bj_21553.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Bj_21553 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String p = br.readLine();
+        System.out.print(p);
+    }
+}

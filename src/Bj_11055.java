c46a695ee2dc8d2e15d185b086d36e05ba2366e0
박병률 <bangrr25@entropykorea.com>
2025-07-08 09:12:53 +0900

    [ADD] 11055

---
+++ src/Bj_11055.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+        int ans = dp[0];
+        for (int it : dp) {
+            ans = Math.max(ans, it);
+        }
+        System.out.print(ans);
+    }
+}

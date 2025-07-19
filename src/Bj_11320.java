21faecaeed90ff1f2dd14a166859176a9ad6d342
Mark <qudfbftnqls@naver.com>
2025-07-20 04:05:14 +0900

    [ADD] 11320

---
+++ src/Bj_11320.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_11320 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a*a/b/b).append("\n");
+        }
+        System.out.print(sb);
+    }
+}

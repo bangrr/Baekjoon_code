6a6c54839e8eea018a82b96966f78f2c1fb36600
Mark <qudfbftnqls@naver.com>
2025-07-21 00:04:26 +0900

    [ADD] 20944

---
+++ src/Bj_20944.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Bj_20944 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print("a".repeat(n));
+    }
+}

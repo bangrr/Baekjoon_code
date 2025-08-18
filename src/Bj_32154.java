1ec93c3adba488b06cdb2b92a9e1752c6fb4c0c4
Mark <qudfbftnqls@naver.com>
2025-08-18 20:54:27 +0900

    [ADD] 32154

---
+++ src/Bj_32154.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Bj_32154 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A B C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A B C F G H L M"}};
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(board[n-1][0]);
+        System.out.print(board[n-1][1]);
+    }
+}

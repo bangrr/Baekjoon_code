0e9d4883ef171223d74e662bcaf681a91c954b64
Mark <qudfbftnqls@naver.com>
2025-08-24 21:47:05 +0900

    [ADD] 16478

---
+++ src/Bj_16478.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_16478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.print((double)a*c/b);
+    }
+}

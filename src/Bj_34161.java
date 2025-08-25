e8869090f0d434c76a4d7b1bb1e20953b89433e0
박병률 <bangrr25@entropykorea.com>
2025-08-25 14:40:11 +0900

    [ADD] 34161

---
+++ src/Bj_34161.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_34161 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 10000; i++) {
+            sb.append("-1\n");
+        }
+        sb.append("-1");
+        System.out.print(sb);
+    }
+}

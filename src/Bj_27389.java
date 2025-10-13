65b1e010d0a78f4a89e5ba76ca9237bf0802daf5
Mark <qudfbftnqls@naver.com>
2025-10-13 23:52:33 +0900

    [ADD] 27389

---
+++ src/Bj_27389.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_27389 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(Integer.parseInt(br.readLine())/4.0);
+    }
+}

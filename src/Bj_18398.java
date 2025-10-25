7548ad8e3192405c3b4ff814cfb0e6fa11146066
Mark <qudfbftnqls@naver.com>
2025-10-25 22:13:43 +0900

    [ADD] 18398

---
+++ src/Bj_18398.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_18398 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                sb.append(a + b).append(" ").append(a * b).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}

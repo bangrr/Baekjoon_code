feed511d4fd6c9970d88dc0739289a1f0ed9e695
Mark <qudfbftnqls@naver.com>
2025-12-04 22:54:27 +0900

    [ADD] 28648

---
+++ src/Bj_28648.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_28648 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = 200;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            m = Math.min(m, a + b);
+        }
+        System.out.print(m);
+    }
+}

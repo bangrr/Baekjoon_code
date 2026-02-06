82f983e30509581053349e751bae7baee9cd764c
Mark <qudfbftnqls@naver.com>
2026-02-06 23:28:28 +0900

    [ADD] 34721

---
+++ src/Bj_34721.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_34721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print("I love DGU\n".repeat(n));
+    }
+}

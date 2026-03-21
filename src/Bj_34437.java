66a6a31bbbee863dd3f3247ca9f1ae8bf272ee68
Mark <qudfbftnqls@naver.com>
2026-03-21 23:20:13 +0900

    [ADD] 34437

---
+++ src/Bj_34437.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_34437 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}

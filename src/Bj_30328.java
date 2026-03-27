f3ac70e6c0ce33aeac429fa674b07304e3c57f6d
Mark <qudfbftnqls@naver.com>
2026-03-27 23:46:44 +0900

    [ADD] 30328

---
+++ src/Bj_30328.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Bj_30328 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(n*4000);
+    }
+}
